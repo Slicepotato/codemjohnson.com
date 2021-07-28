951dc50c0216f8abc2cdc9f5e955aad36da56c37
Michael Johnson <michael.johnson@vividimpact.com>
2021-07-27 21:03:17 -0400

    init stable

---
+++ interactive/src/components/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">
+            {{ post.title }}
+        </h1>
+        <article v-html="post.content">
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    computed: {
+        post() {
+            return {
+                title: 'test post',
+                slug: 'test-post',
+                content: '<p>this is a test post</p>',
+                excerpt: '<p>this is a test post</p>'
+            }
+        }
+    }
+})
+</script>
---
+++ interactive/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div>
+        <h1 class="text-center">Michael Johnson</h1>
+        <div class="container">
+          <div class="row">
+            <PostCard v-for="post in posts" :key="post.slug" :post="post" />
+            <div>
+              <div v-if="pageInfo.hasNextPage">
+                <router-link :to="{ query: { after: pageInfo.enCursor } }">
+                  <span>Next Page</span>
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import PostCard from '@/components/PostCard.vue'
+
+export default Vue.extend({
+  components: { PostCard },
+  computed: {
+    posts() {
+      return [
+        {
+          title: 'test post',
+          slug: 'test-post',
+          content: '<p>this is a test post</p>',
+          excerpt: '<p>this is a test post</p>'
+        },
+      ]
+    },
+    pageInfo() {
+      return {
+        endCursor: '',
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: '',
+      }
+    }
+  }
+})
+</script>

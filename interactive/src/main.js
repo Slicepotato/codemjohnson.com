4de2ba0211e7a7307de526c2fbbfe826af75c4a2
Michael Johnson <michael.johnson@vividimpact.com>
2021-07-28 12:40:32 -0400

    stable REST api

--- interactive/src/main.js
+++ interactive/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 import router from './router'
 import store from './store'
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'http://wp.codemjohnson.com/wp-json';
 
 Vue.config.productionTip = false
 
--- interactive/src/views/Home.vue
+++ interactive/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div v-for="item in page" :key="item">
+     <h1>{{ item.title.rendered }}</h1>
+     <div class="page-content" v-html="item.content.rendered"></div>
+   </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      slug: this.$route.name.replace(/\s+/g, '-').toLowerCase(),
+      page: []
+    }
+  },
   components: {
-    HelloWorld
-  }
+    
+  },
+  created: function() {
+    this.$http.get('wp/v2/pages?slug=' + this.slug).then(response => {
+      for(let item in response.data){
+        this.page.push(response.data[item]);
+      }
+      console.log(response);
+    }, error => { 
+      alert(error) 
+    });
+  },
+  computed: {
+    currentRouteName() {
+        return this.$route.name;
+    }
+}
 }
 </script>

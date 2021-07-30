5c7f4422b0f0876c9aed0b05e1eb1851d2019ed2
Michael Johnson <michael.johnson@vividimpact.com>
2021-07-30 14:02:57 -0400

    working on dynamic menu calls

--- interactive/src/components/Footer.vue
+++ interactive/src/components/Footer.vue
@@ -2,20 +2,21 @@
     <footer>
         <div class="content-wrap flex flex--justify-between flex--align-items-start flex--column">
             <div id="signature">
-				<h3>{{ user }}</h3>
+				<h3>{{ getUser('wp_x5xwdr') }}</h3>
 			</div>
 			<div class="footerMeta flex flex--justify-between flex--align-items-start flex--column">
                 <ul class="flex flex--justify-between flex--align-items-start flex--column">
-                    <li v-for="menu in stuffTitle" :key="menu"><h3>{{ menu.name }}</h3></li>
-                    <li v-for="item in stuff" :key="item">
+                    <li><h3>{{ getMenuTitle(2) }}</h3></li>
+                    {{getMenu(2)}}
+                    <li v-for="item in getMenu(2)" :key="item">
                         <h3>
-                            <a :href="item.url" :target="item.target">{{ item.title }}</a>
+                            <a :href="item.url" :target="item.target">{{ item }}</a>
                         </h3>
                     </li>
                 </ul>
 				<ul class="flex flex--justify-between flex--align-items-start flex--column">
-					<li v-for="menu in contactTitle" :key="menu"><h3>{{ menu.name }}</h3></li>
-					<li v-for="item in contact" :key="item">
+					<li><h3>{{ getMenuTitle(3) }}</h3></li>
+					<li v-for="item in getMenu(3)" :key="item">
                         <h3>
                             <a :href="item.url" :target="item.target">{{ item.title }}</a>
                         </h3>
@@ -29,15 +30,12 @@
 <script>    
 export default {
     name: 'Footer',
-    data () {
-        let user;
-        
+    data () {     
         return {
             slug: this.$route.name.replace(/\s+/g, '-').toLowerCase(),
-            stuff: [],
-            contact: [],
-            menuTitle: [],
-            user
+            menuTitles: [],
+            menuItems: [],
+            userData: []
         }
     },
     components: {
@@ -45,50 +43,59 @@ export default {
     },
     created: function() {
         this.$http.get('wp/v2/menu/').then(response => {
-            for(let item in response.data){
-                this.menuTitle.push(response.data[item]);
-            }
-            console.log(response);
+            for(let title in response.data){
+                this.menuTitles.push(response.data[title]);
+                this.getMenuItems(response.data[title].term_id);
+                // console.log(this.menuItems);
+            }        
         }, error => { 
             alert(error) 
         });
 
-        this.$http.get('wp/v2/menu/2').then(response => {
-            for(let item in response.data){
-                this.stuff.push(response.data[item]);
-            }
-            // console.log(response);
-        }, error => { 
-            alert(error) 
-        });
-
-        this.$http.get('wp/v2/menu/3').then(response => {
-            for(let item in response.data){
-                this.contact.push(response.data[item]);
-            }
-            // console.log(response);
-        }, error => { 
-            alert(error) 
-        });
-
-        this.$http.get('wp/v2/users/1').then(response => {
-            let userObj = response.data;
-            this.user = userObj.name;
-            // console.log(userObj);
+        this.$http.get('wp/v2/users/').then(response => {
+            this.userData = response.data;
         }, error => { 
             alert(error) 
         });
     },
+    mounted() {
+        
+    },
     computed: {
         currentRouteName() {
             return this.$route.name;
+        },        
+    },
+    methods: {
+        getMenuTitle(menuId) {
+            let menuName = this.menuTitles.find(i => i.term_id === menuId);
+            return menuName.name;
         },
-        stuffTitle: function () {
-            return this.menuTitle.filter(i => i.term_id === 2)
+        getUser(userId) {
+            if(this.userData) {
+                let targetUser = this.userData.find(user => user.slug == userId);
+                return targetUser.name;
+            }
         },
-        contactTitle: function () {
-            return this.menuTitle.filter(i => i.term_id === 3)
+        getMenuItems(menuId) {
+            let menuList = this.$http.get('wp/v2/menu/' + menuId ).then(response => {
+                let menu = [];
+                let menuIdTerm = {
+                    'term_id': menuId
+                }
+                menu.push(menuIdTerm)
+                menu.push(response.data);
+                this.menuItems.push(menu);                   
+            }, error => { 
+                alert(error) 
+            });
         },
+        getMenu(menuId) {
+            let menu = this.menuItems.filter(i => i[0].term_id == menuId);
+            console.log(innerMenu)
+
+            return this.menuItems.find(i => i.term_id === menuId);
+        }
     }
 }
 </script>

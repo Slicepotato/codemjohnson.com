823a74090c1d43942b5c6ba11b7d6199cd453c38
Michael Johnson <michael.johnson@vividimpact.com>
2021-08-04 16:19:40 -0400

    corrected tag collection | final

--- interactive/src/components/Experience.vue
+++ interactive/src/components/Experience.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="slick-wrap">
-        {{ jobsInfo }}
         <slick ref="slick" :options="slickOptions"  v-if="jobsInfo.length > 0">
             <template v-for="job in jobsInfo">
                 <div class="card" :key="job">
@@ -11,10 +10,17 @@
                   <h4 class="employer" v-else>{{ job.title.rendered}}</h4>
                   <p class="job-title">{{ job.acf.job_title }}</p>
                   <p class="job-excerpt" v-html="job.excerpt.rendered"></p>
-                  {{ job.tagList }}
-                  <ul v-for="tag in job.tagList" :key="tag">
-                    <li>blah blah blah</li>
-                  </ul>
+                  <template v-if="job.tagList.length">
+                    <h5>Skills</h5>
+                    <ul v-for="tag in job.tagList" :key="tag">
+                      <li>
+                        <span>{{ tag.name }}</span>
+                      </li>
+                    </ul>
+                  </template>
+                  <template v-else>
+                    <p>nope</p>
+                  </template>
                 </div>
             </template>
         </slick>
@@ -26,28 +32,29 @@ import Slick from 'vue-slick';
 import 'slick-carousel/slick/slick.css';
 
 export default {
-    name: 'Experience',
-    components: { Slick },
-    data() {
-        return {
-            slickOptions: {
-                slidesToShow: 3,
-                infinite: false,
-                accessibility: true,
-                adaptiveHeight: true,
-                arrows: true,
-                dots: true,
-                draggable: true,
-                edgeFriction: 0.30,
-                swipe: true
-                // Any other options that can be got from plugin documentation
-            },
-            jobsInfo: [],
-        };
-    },
-    created: function() {
-      this.init();    
-    },
+  name: 'Experience',
+  components: { Slick },
+  data() {
+      return {
+          slickOptions: {
+              slidesToShow: 3,
+              infinite: false,
+              accessibility: true,
+              adaptiveHeight: true,
+              arrows: true,
+              dots: true,
+              draggable: true,
+              edgeFriction: 0.30,
+              swipe: true
+              // Any other options that can be got from plugin documentation
+          },
+          jobsInfo: [],
+      };
+  },
+  created: function() {
+    this.init(); 
+    // this.fetchEmployment();   
+  },
   methods: {
     next() {
       this.$refs.slick.next()
@@ -58,38 +65,53 @@ export default {
     reInit() {
         // Helpful if you have to deal with v-for to update dynamic lists
         this.$nextTick(() => {
-            this.$refs.slick.reSlick();
+            this.$refs.slick.reSlick(); 
         });
     },
     init: function(){
 			this.fetchEmployment();
 		},
     fetchEmployment() {
-      this.$http.get('wp/v2/employment').then((response) => {    
-        this.jobsInfo = response.data;
-        
-        for(let job in response.data){
-          this.jobsInfo[job]['tagList'] = []
-          for(let tagIds in response.data[job].acf.skills_list){
-            this.fetchTags(response.data[job].acf.skills_list[tagIds]).then(function(result){
-              this.jobsInfo[job]['tagList'].push({'name':result.name});
-            });
-          }
-        }
+      this.$http.get('wp/v2/employment').then((response) => {  
+        const jobsInfo = response.data;
 
-        console.log(this.jobsInfo);
-      }, error => { 
-        alert(error) 
+        const tagInit = this.initTagArray(jobsInfo);
+
+        this.addTagArray(tagInit);
+
+        this.jobsInfo = jobsInfo;
+
+      }).catch((error) => { 
+        alert(error);
       });
     },
     fetchTags(tagList) {
       return this.$http.get('wp/v2/tags/' + tagList).then((response) => {
         return response.data;
-      }, error => { 
+      }).catch((error) => { 
         alert(error) 
       });
+    },
+    initTagArray(jobsArray){
+      for(let job in jobsArray){
+        jobsArray[job]['tagList'] = [];
+      }
+      
+      return jobsArray;
+    },
+    async addTagArray(jobsArray){
+      for(let job in jobsArray){
+        for(let tag in jobsArray[job].acf.skills_list) {
+          const res = await this.fetchTags(jobsArray[job].acf.skills_list[tag]);
+          jobsArray[job]['tagList'].push({'name':res.name}); 
+        }
+      }
     }
+  },
+  mounted: function(){
+    // this.fetchEmployment();
   }
+  
 }
 </script>
 

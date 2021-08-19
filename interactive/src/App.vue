f5e3a6e9be82d4af239c6df9dd1437fa7691a4d3
Michael Johnson <m.ianjohnson@gmail.com>
2021-08-18 22:01:45 -0400

    loader update

--- interactive/src/App.vue
+++ interactive/src/App.vue
@@ -1,47 +1,38 @@
 <template>
   <div id="app" :style="{ 'background-image':'url(' + pageBg +')'}">
-    <div id="load-wrapper" class="load-wrapper" :class="[loading ? 'init' : '']">
-      <div class="loading" v-if="loading">
-        <h1>loading</h1>
-        <p id="percent" ref="percent" class="loading-text">{{ percent }}</p>
-        <span id="bar" ref="bar" class="loading-bar" :style="{ 'width':percent }"></span>
-      </div>
-    </div>
-    <Header/>
+    
+    <Header />
     <div class="page-wrap" :style="{ 'background-image':'url(' + footerBg +')'}">
-      <router-view/>
+      <router-view />
     </div>
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+import PageLoad from '@/components/PageLoad.vue';
 
 let pageBg;
 let footerBg;
-let percent;
 
 export default {
   data () {
     return {
       footerBg,
       pageBg,
-      loading: false,
-      percent,
     }
   },
   components: {
     Footer,
     Header,
+    PageLoad
   },
   created: function() {
     this.init();    
-    this.loading = true;
   }, 
   mounted: function() {    
-    this.pageLoad();
   },
   methods: {
     init: function(){
@@ -61,85 +52,6 @@ export default {
           alert(error) 
       });
     },
-    pageLoad() {
-      let i = 0; 
-      let k = 20;    
-      const self = this; 
-      setInterval(function(){
-        if(i === 21)  {
-          clearInterval(this);
-          setTimeout(function(){ 
-            setInterval(function(){
-              if(k === 101)  {
-                clearInterval(this);
-                setTimeout(function(){ 
-                  self.loading = false;
-                }, 500);
-              } else {
-                self.percent = k + '%';
-                k++;
-              }
-            }, 10);
-          }, 500);
-        } else {
-          self.percent = i + '%';
-          i++;
-        }
-      }, 25);
-    }
   }
 }
 </script>
-
-<style lang="scss">
-  @import '@/assets/scss/utility/_variables.scss';
-
-  .load-wrapper {
-    .loading {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      max-width: $sm;
-      width: 100%;
-      text-align: center;
-      padding: 1rem;
-
-      h1 {
-        font-weight: 300;
-        color: $accent-orange;
-        font-size: 3em;
-        margin: 0;
-        line-height: .25;
-      }
-      
-      .loading-text {
-        font-weight: 700;
-        font-size: 5em;
-        line-height: 1;
-        margin: .25em 0 0;
-        color: $grey-8;
-      }
-      
-      .loading-bar {
-        display: block;
-        height: 2px;
-        background-color: $accent-orange;
-        width:0;
-      }
-    }
-
-    &.init {
-      position: fixed;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      z-index: 999;
-      transition: all .25s ease-in-out;
-      background-color: $blk;
-    }
-  }
-</style>
